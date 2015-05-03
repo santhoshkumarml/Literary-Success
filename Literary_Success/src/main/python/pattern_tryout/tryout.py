43e3d80a25b99a81e7bed84e2c5a8ad3c91fad10
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-05-03 02:08:57 -0400

    changess

--- Literary_Success/src/main/python/pattern_tryout/tryout.py
+++ Literary_Success/src/main/python/pattern_tryout/tryout.py
@@ -9,7 +9,37 @@ from pattern.en import tag
 # s = parsetree('The cat sat on the mat.', relations=True, lemmata=True)
 # print repr(s)
 
-from pattern.en import parse
-s = 'This is my sample'
-s = parse(s, relations=True, lemmata=True)
-print s
+# from pattern.en import parse
+# s = 'This is my sample'
+# s = parse(s, relations=True, lemmata=True)
+# print s
+
+
+
+from nltk.corpus import wordnet as wn
+
+
+
+def getSenseSimilarity(worda,wordb):
+	wordasynsets = wn.synsets(worda)
+	wordbsynsets = wn.synsets(wordb)
+
+	synsetnamea = [wn.synset(str(syns.name)) for syns in wordasynsets]
+
+	synsetnameb = [wn.synset(str(syns.name)) for syns in wordbsynsets]
+
+	for sseta, ssetb in [(sseta,ssetb) for sseta in synsetnamea\
+
+	for ssetb in synsetnameb]:
+
+		pathsim = sseta.path_similarity(ssetb)
+
+		wupsim = sseta.wup_similarity(ssetb)
+
+		if pathsim != None:
+			print "Path Sim Score: ",pathsim," WUP Sim Score: ",wupsim,\
+			"\t",sseta.definition, "\t", ssetb.definition
+
+
+
+getSenseSimilarity('cricket','score')
