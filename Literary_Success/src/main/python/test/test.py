3d58b1bb2791505fb15a3341aa323ab924a728cb
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-05-03 21:25:12 -0400

    Refactor

--- Literary_Success/src/main/python/test/test.py
+++ Literary_Success/src/main/python/test/test.py
@@ -1,7 +1,7 @@
 __author__ = 'santhosh'
 
 
-from predict_with_pos_dist import NaivePOSDictPredictor
-from predict_with_pos_dist import NaiveConsTreePredictor
+from feature_wise_predictor import NaivePOSDictPredictor
+from feature_wise_predictor import NaiveConsTreePredictor
 
 print NaiveConsTreePredictor.doClassification()
