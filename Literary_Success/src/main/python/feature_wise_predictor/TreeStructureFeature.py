57bf45374c4729f70a8bacc54f9959e6b6cccc8a
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-05-04 18:30:40 -0400

    Changes

--- Literary_Success/src/main/python/feature_wise_predictor/TreeStructureFeature.py
+++ Literary_Success/src/main/python/feature_wise_predictor/TreeStructureFeature.py
@@ -3,8 +3,8 @@ __author__ = 'santhosh'
 from util import NovelMetaGenerator
 from nltk.tree import ParentedTree
 from util import ml_util
+from util import utils
 import numpy
-import random
 import re
 
 SENTENCES = 'sentences'
@@ -16,7 +16,7 @@ TUPLES = 'tuples'
 def getSuccessFailure():
     core_nlp_files_dict = NovelMetaGenerator.listGenreWiseFileNames(\
         NovelMetaGenerator.CORE_NLP_BASE,\
-        NovelMetaGenerator.SYNSET_WSD_FILE_SUFFIX)
+        NovelMetaGenerator.SYNSET_WSD_TAG_PATTERN)
     success_files, failure_files = {}, {}
     for x in core_nlp_files_dict.keys():
         W, L = [], []
@@ -49,13 +49,16 @@ def getTree(core_nlp_files_dict, genre):
                 parsetree = sent[PARSE_TREE]
                 t = ParentedTree.fromstring(parsetree)
                 product[parsetree] = t
-        productions[files[0]] = product
+        key = files[0].replace(NovelMetaGenerator.CORE_NLP_FILE_SUFFIX, '')
+        productions[key] = product
     return productions
 
 
 def doVarianceMeasure(core_nlp_files_dict, genre):
+    meta_dict = NovelMetaGenerator.loadInfoFromMetaFile()
+    meta_dict_for_genre = meta_dict[genre]
     productions = getTree(core_nlp_files_dict, genre)
-    for files in productions.keys():
+    for files in meta_dict_for_genre.keys():
         product = productions[files]
         rules = []
         diff_productions = {}
@@ -69,10 +72,12 @@ def doVarianceMeasure(core_nlp_files_dict, genre):
     return (numpy.mean(rules), numpy.std(rules), numpy.var(rules))
 
 def doComplexCompound(core_nlp_files_dict, l, genre):
+    meta_dict = NovelMetaGenerator.loadInfoFromMetaFile()
+    meta_dict_for_genre = meta_dict[genre]
     productions = getTree(core_nlp_files_dict, genre)
     featureV =[]
     label = []
-    for files in productions.keys():
+    for files in meta_dict_for_genre.keys():
         product = productions[files]
         types = {'SIMPLE':0, 'COMPLEX':0, 'COMPOUND':0, 'COMPLEX-COMPOUND':0, 'OTHER':0}
         for sents in product.keys():
@@ -103,28 +108,58 @@ def checkProduction(tree):
     return 'OTHER'
 
 
-def shuffle(data, labels):
-    start = len(data) - (6*len(data)/10)
-    end = len(data) - (5*len(data)/10)
-    x = [i for i in range(len(data))]
-    random.shuffle(x)
-    traindata = [data[i] for i in x[:start]]
-    traindata.extend([data[i] for i in x[end:]])
-    trainlabels = [labels[i] for i in x[:start]]
-    trainlabels.extend([labels[i] for i in x[end:]])
-    testdata = [data[i] for i in x[start:end]]
-    testlabels = [labels[i] for i in x[start:end]]
-    return (traindata,trainlabels,testdata, testlabels)
 
-#doClassification()
-meta_dict = NovelMetaGenerator.loadInfoFromMetaFile()
-x, y = getSuccessFailure()
-for genre in x:
-    if genre == 'Science Fiction' or genre == 'Short Stories':
-        continue
-    fv1, lb1 = doComplexCompound(x,'S',genre)
-    fv2, lb2 = doComplexCompound(y,'F',genre)
-    fv1.extend(fv2)
-    lb1.extend(lb2)
-    # ml_util.splitTrainAndTestData(meta_dict[genre], )
-    # print genre, "Accuracy:", accuracy_score(label, mylabel)
+def extractHeightDistributionFeature(core_nlp_files):
+    max_ht = 0
+    height_distribution_feature = dict()
+    for genre_file_path, genre_file_name in core_nlp_files:
+        dictionary = dict()
+        with open(genre_file_path) as f:
+            lines = f.readlines()
+            assert len(lines) == 1
+            line = lines[0]
+            line = 'dictionary=' + line
+            exec(line)
+            # print genre_file_path, dictionary
+            sentences = dictionary[NovelMetaGenerator.SENTENCES]
+            height_feature_for_file = dict()
+            for sent in sentences:
+                parsetree = sent[NovelMetaGenerator.PARSE_TREE]
+                t = ParentedTree.fromstring(parsetree)
+                height = t.height()
+                if height > max_ht:
+                    max_ht = height
+                if height not in height_feature_for_file:
+                    height_feature_for_file[height] = 0.0
+                height_feature_for_file[height] += 1.0
+
+            key = genre_file_name.replace(NovelMetaGenerator.CORE_NLP_FILE_SUFFIX, '')
+            height_distribution_feature[key] = height_feature_for_file
+    height_distribution_feature = utils.normalize_dist(height_distribution_feature, set([i for i in range(1, max_ht)]))
+    return height_distribution_feature
+
+
+def doClassification():
+    meta_dict = NovelMetaGenerator.loadInfoFromMetaFile()
+    core_nlp_files_dict = NovelMetaGenerator.listGenreWiseFileNames(NovelMetaGenerator.CORE_NLP_BASE,\
+                                                                    NovelMetaGenerator.CORE_NLP_TAG_FILES_PATTERN)
+    # x, y = getSuccessFailure()
+    # for genre in x:
+    #     if genre == 'Science Fiction' or genre == 'Short Stories':
+    #         continue
+    #     fv1, lb1 = doComplexCompound(x,'S',genre)
+    #     fv2, lb2 = doComplexCompound(y,'F',genre)
+    #     fv1.extend(fv2)
+    #     lb1.extend(lb2)
+    for genre in meta_dict.keys():
+        if genre == 'Science Fiction' or genre == 'Short Stories':
+            continue
+        meta_dict_for_genre = meta_dict[genre]
+        core_nlp_files = core_nlp_files_dict[genre]
+        feature_dict = extractHeightDistributionFeature(core_nlp_files)
+        train_data, train_result, test_data, test_result =\
+            ml_util.splitTrainAndTestData(meta_dict_for_genre, feature_dict)
+        accuracy = ml_util.doClassfication(train_data, train_result, test_data, test_result)
+        print genre, ':', accuracy
+
+doClassification()
